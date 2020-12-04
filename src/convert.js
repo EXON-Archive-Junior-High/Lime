2ed848520a94851e441f700431938dda05aab6b3
시혁 박 <exon.indiv@gmail.com>
2020-12-04 21:12:31 +0900

    aspirated2simple

--- src/convert.js
+++ src/convert.js
@@ -2,4 +2,14 @@ function enter2space(str) {
     return str.replace('\n', ' ').replace('    ', '')
 }
 
+function aspirated2simple(consonant) {
+    if (consonant == 'ㄲ' || consonant == 'ㅋ') return 'ㄱ'
+    else if (consonant == 'ㄸ' || consonant == 'ㅌ') return 'ㄷ'
+    else if (consonant == 'ㅃ' || consonant == 'ㅍ') return 'ㅂ'
+    else if (consonant == 'ㅆ') return 'ㅅ'
+    else if (consonant == 'ㅉ' || consonant == 'ㅊ') return 'ㅈ'
+    return consonant
+}
+
 module.exports.enter2space = enter2space
+module.exports.aspirated2simple = aspirated2simple
--- src/rhyme/alliteration.js
+++ src/rhyme/alliteration.js
@@ -1,20 +1,21 @@
 const hangul = require('hangul-js')
-const { enter2space } = require('../convert')
+const { enter2space, aspirated2simple } = require('../convert')
 
 function alliteration() {
     const str = enter2space(`기나긴 기다림과 고난의 끝, 가사적
     권능으로 구름을 걷으며 길거리를 걷는`)
-    const arr = str.split(' ')
+    const charArr = str.split('')
+    const firstChar = []
+    let j = 0
+    for (let i = 0; i < charArr.length; i++) {
+        if (charArr[i] == ' ') {
+            firstChar[j] = aspirated2simple(hangul.disassemble(charArr[i + 1])[0])
+            console.log(firstChar[j])
+            j++
+        }
+    }
+    console.log(j)
     
-    let disassembleWords = []
-    
-    arr.forEach((word) => {
-        const disassembleWord = hangul.disassemble(word, true)
-        disassembleWords.append(word)
-        disassembleWord.forEach((j) => {
-            console.log(j)
-        })
-    })
 }
 
 module.exports.alliteration = alliteration
