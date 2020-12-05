d55693f21cbe0e4fe5cfc130bd58698ad10fcc31
시혁 박 <exon.indiv@gmail.com>
2020-12-05 14:44:34 +0900

    alliteration

--- src/rhyme/alliteration.js
+++ src/rhyme/alliteration.js
@@ -1,21 +1,28 @@
 const hangul = require('hangul-js')
+const chalk = require('chalk')
 const { enter2space, aspirated2simple } = require('../convert')
 
 function alliteration() {
-    const str = enter2space(`기나긴 기다림과 고난의 끝, 가사적
-    권능으로 구름을 걷으며 길거리를 걷는`)
-    const charArr = str.split('')
+    const str = enter2space(`아냐 흘러가는 것은 흘러가는 대로 현재의 우리의 멋있는 모습`)
+    console.log(chalk.red(str))
+    let charArr = str.split('')
+    console.log(charArr)
     const firstChar = []
     let j = 0
     for (let i = 0; i < charArr.length; i++) {
-        if (charArr[i] == ' ') {
-            firstChar[j] = aspirated2simple(hangul.disassemble(charArr[i + 1])[0])
-            console.log(firstChar[j])
+        if (charArr[i] === ' ') {
+            firstChar[j] = [aspirated2simple(hangul.disassemble(charArr[i + 1])[0]), i + 1]
+            j++
+        } else if (i === 0) {
+            firstChar[j] = [aspirated2simple(hangul.disassemble(charArr[i])[0]), i]
             j++
         }
     }
-    console.log(j)
+    for (let i = 0; i < firstChar.length; i++) {
+        charArr[firstChar[i][1]] = chalk.blue('[' + charArr[firstChar[i][1]] + ']')
+    }
     
+    console.log(charArr.join(''))
 }
 
 module.exports.alliteration = alliteration
